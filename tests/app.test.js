f48eece9225ab3944d57106987d5236fb2eadfb1
kambelkouame <oikambelkouame@gmail.com>
2025-10-01 20:34:28 +0000

    correction

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,4 +1,5 @@
-const { addition } = require('../src/calcul');
+// tests/app.test.js
+const { addition } = require('../src/app');
 
 test('addition de 2 + 3 = 5', () => {
   expect(addition(2, 3)).toBe(5);
